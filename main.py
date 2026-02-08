05e92375d18efa83c99439f4bb735da5b7a45b7d
avalonchris95 <avalonchris95@gmail.com>
2026-02-07 23:05:05 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Ask for numbers
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Convert to numbers
+num1 = float(num1)
+num2 = float(num2)
+
+# Add them
+sum = num1 + num2
+
+# Show result
+print("The sum is:", sum)
